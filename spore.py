5e635de1422f623c7a14b6cd933684199fc4fd28
Ktwru <spacewarlushd@gmail.com>
2019-10-24 21:43:26 +0300

    another alghorithm
    in previous commit essence constantly growing up, food changing speed of growing. In that commit essense getting resources which providing an apportunity to grow

--- spore.py
+++ spore.py
@@ -18,15 +18,16 @@ def main():
     tic = 0
     feed = 1
     font = pygame.font.Font('PixelFont.ttf', 36)
+    free = 5
 
     pygame.display.update()
     while True:
-        pygame.time.delay(300)
+        pygame.time.delay(30)
         window.fill((20, 0, 0))
         field_surface.fill((0, 0, 0))
         field_matrix_copy = copy.deepcopy(field_matrix)
         tic += 1
-        t_feed = font.render('score:' + str(feed), 0, (0, 135, 72))
+        t_feed = font.render('score:' + str(free), 0, (0, 135, 72))
         t_side = font.render('side:' + str(side), 0, (0, 135, 72))
         window.blit(t_feed, (600, 50))
         window.blit(t_side, (600, 100))
@@ -71,10 +72,11 @@ def main():
                                                 side = [(ydif*ydif) + (xdif*xdif), y + 1, x]
         print(side, feed)
         field_matrix[side[1]][side[2]] = 1
+
         if side[0] == 1:
             side[0] = 10001
             feed += 1
-
+            free += 10
         circuit = []             # growing up
         for y in range(50):
             for x in range(50):
@@ -87,10 +89,10 @@ def main():
                         circuit.append([y, x + 1])
                     if field_matrix[y][x - 1] != 1:
                         circuit.append([y, x - 1])
-        for i in range(feed):
+        if free != 0:
             cords = circuit[randint(0, len(circuit) - 1)]
             field_matrix[cords[0]][cords[1]] = 1
-
+            free -= 1
 
         window.blit(field_surface, (0, 0))
         pygame.display.update()
